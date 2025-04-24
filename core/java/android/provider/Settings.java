9d6bd49590750ee89ab19639df44e5883a86b392
Pranav Vashi <neobuddy89@gmail.com>
2025-04-25 00:37:55 +0530

    services: Clean up unused settings for three finger swipe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6734,14 +6734,6 @@ public final class Settings {
          */
 
         /**
-         * Whether three fingers swipe is active
-         * 0 = Inactive, 1 = Active
-         * @hide
-         */
-        @Readable
-        public static final String THREE_FINGER_GESTURE_ACTIVE = "three_fingers_swipe_active";
-
-        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
